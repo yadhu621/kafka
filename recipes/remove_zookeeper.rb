7d76cfdf950c8fc5fc3defde186c04a77f227c96
Yadhu <Yadhu621@gmail.com>
2018-07-01 18:13:45 +0100

    edited remove recipe to add /tmp/zookeeper-3.4.12

--- recipes/remove_zookeeper.rb
+++ recipes/remove_zookeeper.rb
@@ -4,20 +4,28 @@ zookeeper_parent_dir = node['kafka']['zookeeper']['parent_dir'] # /zookeeper
 zookeeper_dataDir = node['kafka']['zookeeper']['dataDir'] # /zookeeper/dataDir
 zookeeper_dataLogDir = node['kafka']['zookeeper']['dataLogDir'] # /zookeeper/dataLogDir
 zookeeper_logs_dir = node['kafka']['zookeeper']['logs_dir'] # /zookeeper/logs
-zookeeper_opt_dir = "/opt/zookeeper"
+zookeeper_opt_dir = '/opt/zookeeper'
+zookeeper_tmp_dir = '/tmp/zookeeper-3.4.12'
 
 # stop zookeeper service
 service 'zookeeper' do
   action :stop
 end
 
-[zookeeper_parent_dir, zookeeper_dataDir, zookeeper_dataLogDir, zookeeper_logs_dir, zookeeper_opt_dir].each do |dir|
+[
+  zookeeper_parent_dir,
+  zookeeper_dataDir,
+  zookeeper_dataLogDir,
+  zookeeper_logs_dir,
+  zookeeper_opt_dir,
+  zookeeper_tmp_dir,
+].each do |dir|
   directory dir do
     action :delete
     recursive true
   end
 end
-  
+
 # remove zookeeper tarball
 file download_destination do
   action :delete
