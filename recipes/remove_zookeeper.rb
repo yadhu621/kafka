77a20ea59165a51592657d32f77ac28d611bca2e
Yadhu <Yadhu621@gmail.com>
2018-07-01 18:04:59 +0100

    added remove recipe to remove /zookeeper folder

--- recipes/remove_zookeeper.rb
+++ recipes/remove_zookeeper.rb
@@ -1,26 +1,25 @@
 # This recipe will remove the current installation of zookeeper
-
-existing_source_with_version = 'zookeeper-3.4.12'
+download_destination = node['kafka']['zookeeper']['download_destination'] # /tmp/zookeeper-3.4.12.tar.gz
+zookeeper_parent_dir = node['kafka']['zookeeper']['parent_dir'] # /zookeeper
+zookeeper_dataDir = node['kafka']['zookeeper']['dataDir'] # /zookeeper/dataDir
+zookeeper_dataLogDir = node['kafka']['zookeeper']['dataLogDir'] # /zookeeper/dataLogDir
+zookeeper_logs_dir = node['kafka']['zookeeper']['logs_dir'] # /zookeeper/logs
+zookeeper_opt_dir = "/opt/zookeeper"
 
 # stop zookeeper service
 service 'zookeeper' do
   action :stop
 end
 
-# remove directory from /tmp
-directory "/tmp/#{existing_source_with_version}" do
-  action :delete
-  recursive true
+[zookeeper_parent_dir, zookeeper_dataDir, zookeeper_dataLogDir, zookeeper_logs_dir, zookeeper_opt_dir].each do |dir|
+  directory dir do
+    action :delete
+    recursive true
+  end
 end
-
-# remove source directory at /opt
-directory '/opt/zookeeper' do
-  action :delete
-  recursive true
-end
-
+  
 # remove zookeeper tarball
-file "/tmp/#{existing_source_with_version}.tar.gz" do
+file download_destination do
   action :delete
 end
 
