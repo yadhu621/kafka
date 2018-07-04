6277f67aa5123f5263b9850cbc1cfd3bdf69303d
Thoppay Prabakaran, Yadhunandan <yadhunandan.thoppayprabakaran@rwe.com>
2018-07-04 17:01:17 +0100

    edited install_scala script

--- files/install_scala.sh
+++ files/install_scala.sh
@@ -6,5 +6,5 @@ if [ $INSTALLED -gt 0 ]; then
         echo "scala is already installed"
 else
         echo "Installing scala " && sleep 2
-        yum install scala -y
+        yum install scala-2.12.2.rpm -y
 fi
